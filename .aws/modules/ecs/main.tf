126bac70c8a9b7809b81fb2d52561ebdd5b1f0f3
Christian Meyer <github@rchristian.de>
2022-06-26 15:34:57 +0200

    make the ECS policies HCL objects to allow (partial) policy validation

--- .aws/modules/ecs/main.tf
+++ .aws/modules/ecs/main.tf
@@ -219,21 +219,46 @@ data "aws_iam_policy_document" "ecs_service_policy" {
 
 /* ecs service scheduler role */
 resource "aws_iam_role_policy" "ecs_service_role_policy" {
-  name = "ecs_service_role_policy"
-  #policy = file("${path.module}/policies/ecs-service-role.json")
+  name   = "ecs_service_role_policy"
   policy = data.aws_iam_policy_document.ecs_service_policy.json
   role   = aws_iam_role.ecs_role.id
+}
+
+data "aws_iam_policy_document" "ecs_task_execution_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
 }
 
 /* role that the Amazon ECS container agent and the Docker daemon can assume */
 resource "aws_iam_role" "ecs_execution_role" {
   name               = "ecs_task_execution_role"
-  assume_role_policy = file("${path.module}/policies/ecs-task-execution-role.json")
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
+}
+
+data "aws_iam_policy_document" "ecs_execution_role_policy" {
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+  }
 }
 
 resource "aws_iam_role_policy" "ecs_execution_role_policy" {
   name   = "ecs_execution_role_policy"
-  policy = file("${path.module}/policies/ecs-execution-role-policy.json")
+  policy = data.aws_iam_policy_document.ecs_execution_role_policy.json
   role   = aws_iam_role.ecs_execution_role.id
 }
 
@@ -300,13 +325,43 @@ resource "aws_ecs_service" "web" {
 Auto Scaling for ECS
 ======*/
 
+data "aws_iam_policy_document" "ecs_autoscale_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["application-autoscaling.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_iam_role" "ecs_autoscale_role" {
   name               = "${var.environment}_ecs_autoscale_role"
-  assume_role_policy = file("${path.module}/policies/ecs-autoscale-role.json")
+  assume_role_policy = data.aws_iam_policy_document.ecs_autoscale_role.json
 }
+
+data "aws_iam_policy_document" "ecs_autoscale_role_policy" {
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "ecs:DescribeServices",
+      "ecs:UpdateService"
+    ]
+  }
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "cloudwatch:DescribeAlarms"
+    ]
+  }
+}
+
 resource "aws_iam_role_policy" "ecs_autoscale_role_policy" {
   name   = "ecs_autoscale_role_policy"
-  policy = file("${path.module}/policies/ecs-autoscale-role-policy.json")
+  policy = data.aws_iam_policy_document.ecs_autoscale_role_policy.json
   role   = aws_iam_role.ecs_autoscale_role.id
 }
 
